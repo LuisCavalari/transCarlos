7fac5cfa5826a891a6576498262fd44e1bcd0f0d
Luis Cavalari <luiscavalari@ufpr.br>
2020-08-08 01:39:32 -0300

    feat: first page added

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const mustache = require('mustache-express')
+const routes = require('./routes')
+const path = require('path')
+
 const app = express()
 
+app.use(routes)
 app.engine('mst', mustache())
 app.set('view engine', 'mst')
-app.set('views', __dirname + 'views')
+app.set('views', path.resolve(__dirname, 'view'))
 
 module.exports = app
